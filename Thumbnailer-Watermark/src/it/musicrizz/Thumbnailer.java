8615036825a8d889ea05a4687260c91176db3f67
Grandinetti Giovanni <grandinetti.giovanni13@gmail.com>
2021-04-13 01:36:37 +0200

    .

--- Thumbnailer-Watermark/src/it/musicrizz/Thumbnailer.java
+++ Thumbnailer-Watermark/src/it/musicrizz/Thumbnailer.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package it.musicrizz;
 
 import java.awt.Graphics2D;
@@ -17,7 +13,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author gio
+ * @author Giovanni Grandnetti
  */
 public class Thumbnailer {
 
